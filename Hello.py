d9ddb2d16e8b051e7b8034925f3f65308ca04a57
Aditya <aditya0697@gmail.com>
2017-11-29 00:05:06 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -28,7 +28,6 @@ consumer_secret = 'xxx'
 access_token = 'xxx'
 access_token_secret = 'xxx'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
